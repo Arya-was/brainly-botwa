d7a5ebe012ecd730111a30ba87a4c9dc5d4817de
Ichinose Honami <74448616+Arya-was@users.noreply.github.com>
2022-10-29 19:21:57 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 const baileys = require('@adiwajshing/baileys')
 const { Brainly } = require("brainly-scraper-v2");
-const brain = new Brainly("ph"); //Ganti Sesuai Negara Hosting lu
+const brain = new Brainly("es"); //Ganti Sesuai Negara Hosting lu
 var data;
 
 module.exports = index = async(client, msg) => {
